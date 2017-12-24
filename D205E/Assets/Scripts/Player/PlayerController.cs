a85dcdb132ed76fc7671a5df2caa9d453cfbfd4e
Levi Burton <levi.burton@outlook.com>
2017-12-24 00:25:50 -0600

    started adding selection rectangle

--- D205E/Assets/Scripts/Player/PlayerController.cs
+++ D205E/Assets/Scripts/Player/PlayerController.cs
@@ -2,6 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public static class BoundsExtensions
+{
+    public static Rect ToScreenSpace(this Bounds bounds, Camera camera)
+    {
+        var WorldToCameraMin = camera.WorldToScreenPoint(bounds.min);
+        var WorldToCameraMax = camera.WorldToScreenPoint(bounds.max);
+
+        var x = WorldToCameraMin.x;
+        var y = Screen.height - WorldToCameraMax.y;
+        var width = WorldToCameraMax.x - WorldToCameraMin.x;
+        var height = WorldToCameraMax.y - WorldToCameraMin.y;
+
+        Rect ScreenSpaceObjectRect = new Rect(x, y, width, height);
+                                             
+        return ScreenSpaceObjectRect;
+    }
+}
+
 public class PlayerController : MonoBehaviour
 {
     public Camera PlayerCamera = null;
@@ -12,16 +30,118 @@ public class PlayerController : MonoBehaviour
     public float PanSpeed = 5.0f;
     public float ZoomSpeed = 5.0f;
 
+    public bool bDrawSelectionRectangle;
+    public Rect SelectionRectangle;
+    public Vector3 DragStartPosition;
+    public Vector3 DragEndPositon;
+
+    public List<Rect> RectsToDraw = new List<Rect>();
+    public List<GameObject> SelectableObjects = new List<GameObject>();
+
     void Awake()
     {
     }
 
     void Update()
     {
-        HandleMouse();
+        HandleSelectionDrag();
+        PanCamera();
+        ZoomCamera();
     }
 
-    public void HandleMouse()
+    public void OnGUI()
+    {
+        if (bDrawSelectionRectangle)
+        {
+            GUI.Box(SelectionRectangle, "");
+        }
+
+        foreach (var go in SelectableObjects)
+        {
+            var WorldBounds = go.GetComponent<BoxCollider>().bounds;
+            var ScreenSpaceObjectRect = WorldBounds.ToScreenSpace(PlayerCamera);
+
+            GUI.Box(ScreenSpaceObjectRect, go.name);
+
+            #region DEBUG
+            //Debug.LogFormat("{0}", go.name);
+            //Debug.LogFormat("    World Position: {0}", go.transform.position);
+            //Debug.LogFormat("    World Bounds: {0}", WorldBounds.ToString());
+            //Debug.LogFormat("    World Bounds Min: {0}", WorldBounds.min.ToString());
+            //Debug.LogFormat("    World Bounds Max: {0}", WorldBounds.max.ToString());
+            //Debug.LogFormat("    Camera World to Screen Min: {0}", WorldToCameraMin);
+            //Debug.LogFormat("    Camera World to Screen Max: {0}", WorldToCameraMax);
+            #endregion
+
+        }
+    }
+
+    public List<GameObject> FindGameObjectWithType<T>()
+    {
+        GameObject[] AllGameObjects = FindObjectsOfType<GameObject>();
+        List<GameObject> ObjectsWithType = new List<GameObject>();
+
+        for (int i = 0; i < AllGameObjects.Length; i++)
+        {
+            if (AllGameObjects[i].GetComponent<T>() != null)
+            {
+                ObjectsWithType.Add(AllGameObjects[i]);        
+            }
+        }
+
+        return ObjectsWithType;
+    }
+
+    public bool HandleSelectionDrag()
+    {
+        bool bDragging = false;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            DragStartPosition = Input.mousePosition;
+            DragEndPositon = DragStartPosition;
+            bDrawSelectionRectangle = true;
+            FindGameObjectWithType<ISelectable>();
+
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            bDrawSelectionRectangle = false;
+            SelectionRectangle.Set(0, 0, 0, 0);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            SelectableObjects = FindGameObjectWithType<ISelectable>();
+
+            DragEndPositon = Input.mousePosition;
+            SelectionRectangle.Set(DragStartPosition.x,
+                                   Screen.height - DragStartPosition.y,
+                                   DragEndPositon.x - DragStartPosition.x,
+                                   -1 * ((Screen.height - DragStartPosition.y) - (Screen.height - DragEndPositon.y)));
+
+            Ray RayStartDrag = PlayerCamera.ScreenPointToRay(DragStartPosition);
+            Debug.DrawRay(RayStartDrag.origin, RayStartDrag.direction * 100, Color.red, 1f, false);
+
+            Ray RayEndDrag = PlayerCamera.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(RayEndDrag.origin, RayEndDrag.direction * 100, Color.green, .15f, false);
+
+            bDragging = true;
+        }
+
+        return bDragging;
+    }
+
+    public void ZoomCamera()
+    {
+        var ScrollWheenl = Input.GetAxis("Mouse ScrollWheel");
+
+        if (ScrollWheenl != 0)
+        {
+            PlayerCamera.transform.position += PlayerCamera.transform.forward * ScrollWheenl * ZoomSpeed * Time.deltaTime;
+        }
+    }
+
+    public void PanCamera()
     {
         PreviousMousePosition = CurrentMousePosition;
         CurrentMousePosition = Input.mousePosition;
@@ -44,15 +164,5 @@ public class PlayerController : MonoBehaviour
         }
 
         PlayerCamera.transform.position = TargetPosition;
-   
-    }
-
-    void PanCamera()
-    {
-    }
-
-    void ZoomCamera()
-    {
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
     }
 }
---
+++ D205E/Assets/Scripts/TestItem.cs
@@ -0,0 +1,70 @@
+﻿using cakeslice;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestItem : MonoBehaviour, ISelectable
+{
+    public bool Selected = false;
+    private Outline Outline;
+
+    public void OnSelect()
+    {
+        Selected = true;
+        Outline.enabled = Selected;
+        Outline.color = 1;
+    }
+
+    public void OnDeselect()
+    {
+        Selected = false;
+        Outline.enabled = false;
+    }
+
+    public void OnToggleSelected()
+    {
+        if (IsSelected())
+        {
+            OnDeselect();
+        }
+        else
+        {
+            OnSelect();
+        }
+    }
+
+    public bool IsSelected()
+    {
+        return Selected;
+    }
+
+    public void OnMouseDown()
+    {
+        OnToggleSelected();
+    }
+
+    public void OnMouseEnter()
+    {
+        Outline.enabled = true;
+        if (!IsSelected())
+        {
+            Outline.color = 0;
+        }
+    }
+
+    public void OnMouseExit()
+    {
+        Debug.Log("OnMouseLeave");
+        if (!IsSelected())
+        {
+            Outline.enabled = false;
+        }
+    }
+   
+
+    // Use this for initialization
+    void Start () {
+        Outline = GetComponent<Outline>();
+        OnDeselect();
+	}
+}
